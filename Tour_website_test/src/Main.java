dbed62cd680fa266b5fa3c7f11be64f39a4b9057
srijoni-deloitte <srijochakraborty@deloitte.com>
2022-03-23 13:17:14 +0530

    Update for assignment 2

--- Tour_website_test/src/Main.java
+++ Tour_website_test/src/Main.java
@@ -16,6 +16,7 @@ public class Main {
         driver = new ChromeDriver();
         //driver.manage().window().maximize();
         driver.get(url);
+        //Assignment 1
         By logoXpath = getPath("//img[@alt='logo']");
         By homXpath = getPath(navBarItem.replace("%s", "Home"));
         By flightsXpath = getPath(navBarItem.replace("%s", "flights"));
@@ -32,6 +33,20 @@ public class Main {
         System.out.println(signUpXpath);
         System.out.println(loginXpath);
         System.out.println(agentDropDwon);
+        //Assignment 2
+        By checkin = getPath("//input[@class='checkin form-control form-control-lg border-top-r0']");
+        By checkout = getPath("//input[@class='checkout form-control form-control-lg border-top-l0']");
+        By adultDec = getPath("//div[@class='dropdown-menu dropdown-menu-wrap']//div[@class='dropdown-item'][2]//i[@class='la la-minus']");
+        By adultInc = getPath("//div[@class='dropdown-menu dropdown-menu-wrap']//div[@class='dropdown-item'][2]//i[@class='la la-plus']");
+        By childPlus = getPath("//div[@class='dropdown-menu dropdown-menu-wrap']//div[@class='dropdown-item'][3]//i[@class='la la-plus']");
+        By childminus = getPath("//div[@class='dropdown-menu dropdown-menu-wrap']//div[@class='dropdown-item'][3]//i[@class='la la-minus']");
+        System.out.println(checkin);
+        System.out.println(checkout);
+        System.out.println(adultDec);
+        System.out.println(adultInc);
+        System.out.println(childPlus);
+        System.out.println(signUpXpath);
+        System.out.println(childminus);
     }
 
     static By getPath(String s) {
